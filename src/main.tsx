6a173add784a412e82464c4e2557ef7970d6ea4d
Abhay9326397439 <abhayzombade26@gmail.com>
2026-06-09 15:21:44 +0530

    Fix Vite config, add index.html and main.tsx, remove old vercel.json

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { SolutionTemplatesPage } from './pages/SolutionTemplates/SolutionTemplates'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SolutionTemplatesPage />
+  </React.StrictMode>
+)
